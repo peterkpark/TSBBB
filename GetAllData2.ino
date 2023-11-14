12cc7b090164207755870fe666c40d21d28aa1f7
Erin Coulon <114038407+ecoulon12@users.noreply.github.com>
2023-11-13 21:25:17 -0800

    Update GetAllData2.ino
    button works

--- GetAllData2.ino
+++ GetAllData2.ino
@@ -29,7 +29,7 @@ void setup() {
   byte status = mpu.begin();
   Serial.print(F("MPU6050 status: "));
   Serial.println(status);
-  while(status!=0){ } // stop everything if could not connect to MPU6050
+  //while(status!=0){ } // stop everything if could not connect to MPU6050
   
   Serial.println(F("Calculating offsets, do not move MPU6050"));
   delay(1000);
@@ -41,70 +41,29 @@ void setup() {
 void loop() {
   mpu.update();
 
+  //PORTB 
+
+
   currAccX = mpu.getAccX();
   currAccY = mpu.getAccY();
+  int button = (PINB & (1<<PB4));
   
   if(millis() - timer > 50){
-    if (abs(currAccX - prevAccX) > 0.75) {
-      if (currAccX - prevAccX > 0) { // gyro moved right
-        Serial.println(1);
-      } else {
-        Serial.println(0);
-      }
+    if ((abs(currAccX - prevAccX) > 1.5)||(abs(currAccY-prevAccY) >1.5)) {
+        Serial.println("shoot!");
+        delay(700);
     }
-//    if (currAccY - prevAccY > 0) {
-//      Serial.println(2);
-//    } else {
-//      Serial.println(3);
-//    }
+
+    if (button == 0){
+      PORTB |= (1<<PB3);
+      Serial.println("button pressed");      
+    }
+    else{
+       PORTB |= (1<<PB3);     
+    }
     timer = millis();
+    //delay(700);
     prevAccX = currAccX;
     prevAccY = currAccY;
   }
 }
-
-
-//
-//  if (prevAccX == 0){
-//    prevAccX = 0.001;
-//  }
-//  if (prevAccY == 0) {
-//    prevAccY = 0.001;
-//  }
-//    
-//  if (abs(currAccX - prevAccX)/ prevAccX > THRESHOLD_DIFF || abs(currAccY - prevAccY)/ prevAccY > THRESHOLD_DIFF) { // 30% difference between values
-//  Serial.println(currAccX);
-//  prevAccX = currAccX;
-//
-//  Serial.println(currAccY);
-//  prevAccY = currAccY;
-//  }
-//  if ((mpu.getAccY()- prevAccY)/ prevAccY > 0.2) {
-////    Serial.print("Y: ");
-//    Serial.println(mpu.getAccY());
-//    prevAccY = mpu.getAccY();
-//  }
-//  if ((mpu.getAccZ()- prevAccZ)/ prevAccZ > 0.2) {
-//    Serial.print("Y: ");Serial.println(mpu.getAccY());
-//    prevAccZ = mpu.getAccZ();
-//  }
-//  
-//  if(millis() - timer > 500){ // print data every second
-////    Serial.print(F("TEMPERATURE: "));Serial.println(mpu.getTemp());
-//    Serial.print(("\tX: ");Serial.print(mpu.getAccX());
-//    Serial.print("\tY: ");Serial.print(mpu.getAccY());
-//    Serial.print("\tZ: ");Serial.println(mpu.getAccZ());
-// 
-//    Serial.print(F("GYRO      X: "));
-//    Serial.println(mpu.getGyroX());
-//    Serial.print("\tY: ");Serial.print(mpu.getGyroY());
-//    Serial.print("\tZ: ");Serial.println(mpu.getGyroZ());
-//  
-//    Serial.print(F("ACC ANGLE X: "));Serial.print(mpu.getAccAngleX());
-//    Serial.print("\tY: ");Serial.println(mpu.getAccAngleY());
-//    
-//    Serial.print(F("ANGLE     X: "));Serial.print(mpu.getAngleX());
-//    Serial.print("\tY: ");Serial.print(mpu.getAngleY());
-//    Serial.print("\tZ: ");Serial.println(mpu.getAngleZ());
-//    timer = millis();
-//  }
